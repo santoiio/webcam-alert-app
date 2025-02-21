c00c141b8f75df7c14d308342546e4f8196ef3a3
santoiio <santoyo96@live.com>
2025-02-21 02:19:05 -0600

    Day 37: Save frames with motion detected

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import cv2
 import time
 from emailing import send_email
@@ -7,6 +8,7 @@ time.sleep(2)
 
 first_frame = None
 status_list = []
+count = 1
 # Wait a few seconds to allow the environment to stabilize
 for _ in range(5):
     check, frame = video.read()
@@ -40,6 +42,11 @@ while True:
             rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (105, 255, 0), 2)
             if rectangle.any():
                 status = 1
+                cv2.imwrite(f"images/{count}.png", frame)
+                count += 1
+                all_images = glob.glob("images/*.png")
+                index = int(len(all_images) / 2)
+                image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
